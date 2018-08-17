45aa7d8556318b18d3c91d43d9d5acd51f2454b0
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-08-17 14:05:57 -0300

    Se agrego test para api de bgpview

---
+++ test/file_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "github.com/thoas/go-funk"
+  "io/ioutil"
+  "bufio"
+  "strings"
+  "testing"
+)
+
+func TestFile(t *testing.T) {
+  files, err := ioutil.ReadDir("../datasets")
+  if err != nil {
+    panic(err)
+  }
+
+  var routes [][]string
+  for _, file := range files {
+    path := fmt.Sprintf("../datasets/%s", file.Name())
+
+    inFile, err := os.Open(path)
+    if err != nil {
+      panic(err)
+    }
+
+    scanner := bufio.NewScanner(inFile)
+    scanner.Split(bufio.ScanLines)
+
+    for scanner.Scan() {
+      line := strings.Split(scanner.Text(), "\t")
+      if funk.Contains(line, "5692") && funk.Contains(line, "D") {
+        routes = append(routes, line)
+      }
+    }
+    inFile.Close()
+  }
+  fmt.Println(routes)
+}
