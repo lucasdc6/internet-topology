fff8e391277e1d971bf8f9a2a9f33091fa280677
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-08-19 15:44:22 -0300

    Added functions

--- options/options.go
+++ options/options.go
@@ -8,12 +8,27 @@ import (
   "github.com/lucasdc6/internet-topology/environment"
 )
 
-func CreateGraph(g *graph.Mutable, paths []string) {
+func GraphToJson(g *graph.Mutable, from int) string {
+  var str strings.Builder
+  graph.BFS(g, from, func(v, w int, _ int64) {
+    fmt.Fprintf(&str, "%d,%d\n", v, w)
+  })
+  return str.String()
+}
+
+func AddToGraph(g *graph.Mutable, paths []string, deepLevel int) {
   debugEnv := environment.GetDebugFor("options") || environment.GetDebug()
   for _, path := range paths {
-    var actual, asni int
+    var high, actual, asni int
     asns := strings.Split(path, " ")
-    actuals, asns := asns[0], asns[1:]
+
+    if deepLevel < len(asns) && deepLevel != 0 {
+      high = deepLevel
+    } else {
+      high = len(asns)
+    }
+
+    actuals, asns := asns[0], asns[1:high]
     actual, _ = strconv.Atoi(actuals)
     if debugEnv {
       fmt.Printf("Actual path: %s\n", path)
