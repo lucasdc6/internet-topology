2b83cbffcaf8d0efd5ce73354949b8fddbc98026
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-08-19 01:19:04 -0300

    Added new option

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
   "os"
   "fmt"
+  "strconv"
   "github.com/pborman/getopt/v2"
+  "github.com/yourbasic/graph"
+  "github.com/lucasdc6/internet-topology/options"
+  "github.com/lucasdc6/internet-topology/environment"
+  "github.com/lucasdc6/internet-topology/bgpview/api"
 )
 
 func main() {
+  debug := environment.GetDebugFor("main") || environment.GetDebug()
   // Declare options
+  opAsInfo := getopt.BoolLong("as-info", 'a', "Show Asn info")
   optIx := getopt.BoolLong("ix", 'i', "Show IX connections")
   optHelp := getopt.BoolLong("help", 'h', "Show this help")
   opFull := getopt.BoolLong("full", 'f', "Show all the connections")
@@ -20,11 +27,26 @@ func main() {
     os.Exit(0)
   }
 
-  if *optIx {
+  if debug && *optIx {
     fmt.Println(*optIx)
   }
 
-  if *opFull {
+  if debug && *opFull {
     fmt.Println(*opFull)
+  }
+
+  if *opAsInfo {
+    g := graph.New(99999)
+    asn, err := strconv.ParseInt(getopt.Args()[0], 10, 64)
+    if err != nil {
+      fmt.Println("Error parsing as number")
+      os.Exit(1)
+    }
+    fmt.Printf("Quering for ASN: %d\n", asn)
+    as := api.GetAsnUpstreams(asn)
+    for _, upstream := range as.Data.Ipv4Upstreams {
+      options.CreateGraph(g, upstream.BgpPaths)
+    }
+    fmt.Println(g)
   }
 }
