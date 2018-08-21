fd691d11e77a8aa3fa6ec7869715bff84fdb315e
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-08-21 08:22:50 -0300

    Minirefactor

--- draw/draw.py
+++ draw/draw.py
@@ -12,55 +12,51 @@ def show_help():
     print '    -o | --output    Set output file (required)'
     print '    -h               Show this help'
 
-def main(argv):
-    inputfile = ''
-    outputfile = ''
-    fileformat = 'pdf'
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["format=", "input=","output="])
-    except getopt.GetoptError:
+inputfile = ''
+outputfile = ''
+fileformat = 'pdf'
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:",["format=", "input=","output="])
+except getopt.GetoptError:
+    show_help()
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
         show_help()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
+        sys.exit()
+    elif opt in ("-i", "--input"):
+        inputfile = arg
+    elif opt in ("-o", "--output"):
+        outputfiletemplate = Template("$path.$format")
+        outputfile = arg
+    elif opt == '--format':
+        if arg not in ("png", "pdf"):
+            print 'ERROR: Incorrect format - Availables (pdf, png)\n'
             show_help()
-            sys.exit()
-        elif opt in ("-i", "--input"):
-            inputfile = arg
-        elif opt in ("-o", "--output"):
-            outputfiletemplate = Template("$path.$format")
-            outputfile = arg
-        elif opt == '--format':
-            if arg not in ("png", "pdf"):
-                print 'ERROR: Incorrect format - Availables (pdf, png)\n'
-                show_help()
-                sys.exit(3)
-            fileformat = arg
-    if (outputfile == ''):
-        print 'ERROR: Missing input or output file\n'
-        show_help()
-        sys.exit(1)
+            sys.exit(3)
+        fileformat = arg
+if (outputfile == ''):
+    print 'ERROR: Missing input or output file\n'
+    show_help()
+    sys.exit(1)
 
-    if (inputfile == ''):
-        infile = sys.stdin
-    else:
-        infile = open(inputfile)
+if (inputfile == ''):
+    infile = sys.stdin
+else:
+    infile = open(inputfile)
 
-    data = []
-    for line in infile:
-        if (line=='q'):
-            break
-        f1, f2 = line.split(",")
-        data.append((f1.rstrip(), f2.rstrip()))
-    outputfile = outputfiletemplate.substitute(path=outputfile, format=fileformat)
-    g = nx.Graph()
+data = []
+for line in infile:
+    if (line=='q'):
+        break
+    f1, f2 = line.split(",")
+    data.append((f1.rstrip(), f2.rstrip()))
+outputfile = outputfiletemplate.substitute(path=outputfile, format=fileformat)
+g = nx.Graph()
 
-    g.add_edges_from(data)
-    pos = nx.spring_layout(g, k=0.3*1/np.sqrt(len(g.nodes())), iterations=1)
-    plt.figure(3, figsize=(30, 30))
-    nx.draw(g, pos=pos)
-    nx.draw_networkx_labels(g, pos=pos)
-    plt.savefig(outputfile)
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+g.add_edges_from(data)
+pos = nx.spring_layout(g, k=0.3*1/np.sqrt(len(g.nodes())), iterations=1)
+plt.figure(3, figsize=(30, 30))
+nx.draw(g, pos=pos)
+nx.draw_networkx_labels(g, pos=pos)
+plt.savefig(outputfile)
