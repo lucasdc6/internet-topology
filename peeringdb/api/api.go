53982a56aecaa0ae66531904614049cb3192bb9d
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-08-26 13:27:29 -0300

    More comments

--- peeringdb/api/api.go
+++ peeringdb/api/api.go
@@ -6,19 +6,21 @@ import (
   "github.com/lucasdc6/internet-topology/utils"
 )
 
-// Public api
+// GetAsn - Get as from asn
 func GetAsn(id int64) (asn types.NetworkBody, error error) {
   url := fmt.Sprintf("https://peeringdb.com/api/net?asn=%d", id)
   error = utils.GetGeneric(url, &asn)
   return asn, error
 }
 
+// GetNetwork - Get network from id
 func GetNetwork(id int64) ( network types.NetworkBody, error error) {
   url := fmt.Sprintf("https://peeringdb.com/api/net/%d", id)
   error = utils.GetGeneric(url, &network)
   return network, error
 }
 
+// GetIx - Get ix from id
 func GetIx(id int64) (ix types.IxBody, error error) {
   url := fmt.Sprintf("https://peeringdb.com/api/ix/%d", id)
   error = utils.GetGeneric(url, &ix)
