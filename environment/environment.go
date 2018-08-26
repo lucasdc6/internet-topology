ed90cebd5f86358820787ef37c7d31720750e032
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-08-26 13:38:27 -0300

    Go beautify

--- environment/environment.go
+++ environment/environment.go
@@ -1,24 +1,24 @@
 package environment
 
 import (
-  "os"
-  "fmt"
-  "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
 // GetDebug - Get general debug value from environment variables
 func GetDebug() bool {
-  if len(os.Getenv("INTERNET_TOPOLOGY_DEBUG")) != 0 {
-    return true
-  }
-  return false
+	if len(os.Getenv("INTERNET_TOPOLOGY_DEBUG")) != 0 {
+		return true
+	}
+	return false
 }
 
 // GetDebugFor - Get module debug value from environment variables
 func GetDebugFor(module string) bool {
-  env := fmt.Sprintf("INTERNET_TOPOLOGY_%s_DEBUG", strings.ToUpper(module))
-  if len(os.Getenv(env)) != 0 {
-    return true
-  }
-  return false
+	env := fmt.Sprintf("INTERNET_TOPOLOGY_%s_DEBUG", strings.ToUpper(module))
+	if len(os.Getenv(env)) != 0 {
+		return true
+	}
+	return false
 }
