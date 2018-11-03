e03f88eedb0fa7e2be6d22974baf88b1b44d9c3f
lucasdc6 <lucasdicunzolo@gmail.com>
2018-11-03 19:09:30 -0300

    fix error with multiples calls with same value

--- options/graph.go
+++ options/graph.go
@@ -2,11 +2,12 @@ package options
 
 import (
 	"fmt"
-	"github.com/lucasdc6/internet-topology/bgpview/types"
+	"strings"
+
 	"github.com/lucasdc6/internet-topology/bgpview/api"
+	"github.com/lucasdc6/internet-topology/bgpview/types"
 	"github.com/lucasdc6/internet-topology/environment"
 	graph "gonum.org/v1/gonum/graph/simple"
-	"strings"
 )
 
 // GraphToData - Convert graph to str
@@ -20,21 +21,24 @@ func GraphToData(g *graph.DirectedGraph, from int) string {
 
 // AddToGraph - Add paths array with deepLevel to graph g
 func AddToGraph(g *graph.DirectedGraph, peers []types.AsnUpDownstream, from, deepLevel int) {
-  if deepLevel == 0 {
-    return
-  }
-  debugEnv := environment.GetDebugFor("options") || environment.GetDebug()
-  for _, peer := range peers {
-    g.SetEdge(graph.Edge{F: graph.Node(from), T: graph.Node(peer.Asn)})
-    if (deepLevel != 0) {
-      fmt.Printf("Quering for ASN: %d\n", peer.Asn)
-      as := api.GetAsnUpstreams(peer.Asn)
-      AddToGraph(g, as.Data.Ipv4Upstreams, peer.Asn, deepLevel-1)
-    } else {
-      return
-    }
-  }
-  if debugEnv {
-    fmt.Println(g)
-  }
+	if deepLevel == 0 {
+		return
+	}
+	debugEnv := environment.GetDebugFor("options") || environment.GetDebug()
+	for _, peer := range peers {
+		if g.Node(int64(peer.Asn)) != nil {
+			continue
+		}
+		g.SetEdge(graph.Edge{F: graph.Node(from), T: graph.Node(peer.Asn)})
+		if deepLevel != 0 {
+			fmt.Printf("Quering for ASN: %d\n", peer.Asn)
+			as := api.GetAsnUpstreams(peer.Asn)
+			AddToGraph(g, as.Data.Ipv4Upstreams, peer.Asn, deepLevel-1)
+		} else {
+			return
+		}
+	}
+	if debugEnv {
+		fmt.Println(g)
+	}
 }
