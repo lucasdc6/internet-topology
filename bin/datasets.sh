38bceb46d0d0da625175b4ba29bab32762c4ac03
lucasdc6 <lucasdicunzolo@gmail.com>
2018-12-14 18:14:08 -0300

    Se agrega script para datasets

---
+++ bin/datasets.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+BASE=${1:-datasets}
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+  cat <<HELP
+Usage $0:
+  You can specify the path base as first argument
+HELP
+  exit 1
+fi
+
+URLS=(
+ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
+ftp.ripe.net/ripe/stats/delegated-ripencc-latest
+ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest
+ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest
+ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest
+)
+
+mkdir -p datasets
+
+for URL in ${URLS[@]}; do
+  ZONE=`echo $URL | cut -d. -f2`
+  echo "Quering to $ZONE..."
+  curl -s $URL > $BASE/$ZONE
+  echo -e "Finish $ZONE at $BASE/$ZONE"
+done
+
